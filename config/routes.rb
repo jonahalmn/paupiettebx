75494564e1d736d2eea8c9708e79560018acc8bc
jonahalmn <jonah.allemonne@icloud.com>
2017-03-28 09:51:33 +0200

    utd

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "meals#index"
+
+  devise_scope :user do
+    get '/signout', to: 'devise/sessions#destroy', as: :signout
+  end
 end
--- db/migrate/20170327220237_devise_create_users.rb
+++ db/migrate/20170327220237_devise_create_users.rb
@@ -21,6 +21,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
+      t.boolean :gender
 
       ## Confirmable
       # t.string   :confirmation_token
