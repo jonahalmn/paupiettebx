703fd7c4b0e10221e8effd79d6e3f0d233897942
BastienBah <bastien.bahuet0704@gmail.com>
2017-03-28 16:59:50 +0200

    link_to mon profil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,6 @@ class ApplicationController < ActionController::Base
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :fname, :role, :adress, :gender, :dateofbirth, :phone, :intersts, :city, :description) }
   end
+
+
 end
